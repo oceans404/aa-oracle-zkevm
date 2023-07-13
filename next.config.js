a48f7c46527412bb5c869af8f888fe902967c7b1
Steph <steph@oceans404.com>
2023-07-13 16:38:32 -0700

    enable static export for deployment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   reactStrictMode: true,
-  webpack: config => {
+  webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     return config;
   },
