4ea2bb9c71f5a2d9ce3c904e5cf31d3a632e0fac
Steph <steph@oceans404.com>
2023-07-13 16:14:32 -0700

    base Next.js app with Rainbowkit connected to Polygon zkEVM

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # aa-oracle-zkevm
+
+A starter template for building a dapp with Account Abstraction and a price feed from an oracle on Polygon zkEVM.
+
+## Getting started
+
+### 1. Install dependencies:
+
+```bash
+yarn install
+```
+
+### 2. Create a `.env.local` file by copying the `.env.example` file:
+
+```bash
+cp .env.example .env.local
+```
+
+### 3. Obtain a Wallet Connect Project ID
+
+- Log in to the [Wallet Connect dashboard](https://cloud.walletconnect.com/sign-in) and create a new named project
+- Update the NEXT_PUBLIC_WALLET_CONNECT_PROJECT_NAME variable in the `.env.local` file with your new Wallet Connect Project Name, to match the name you gave your project in the Wallet Connect dashboard
+- Update the NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID variable in the `.env.local` file with your new Wallet Connect Project ID from the dashboard
+
+### Run the development server:
+
+```bash
+yarn run dev
+```
+
+## Next.js frontend bootstrapped with create-rainbowkit
+
+Scaffold a new RainbowKit + wagmi + Next.js app with [`create-rainbowkit`](https://github.com/rainbow-me/rainbowkit/tree/main/packages/create-rainbowkit).
+
+```
+yarn create @rainbow-me/rainbowkit
+```
---
+++ pages/_app.tsx
@@ -0,0 +1,42 @@
+import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import type { AppProps } from "next/app";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { polygonZkEvm, polygonZkEvmTestnet } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true"
+      ? [polygonZkEvmTestnet]
+      : []),
+    polygonZkEvm,
+  ],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_NAME || "",
+  projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || "",
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+  webSocketPublicClient,
+});
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+}
+
+export default MyApp;
