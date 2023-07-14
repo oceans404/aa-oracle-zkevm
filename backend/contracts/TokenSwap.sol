306dca13268e635a48669194a4660169b81cc966
billyjitsu <82428978+billyjitsu@users.noreply.github.com>
2023-07-13 23:57:12 -0700

    adding natspec
    more notes on main code

--- backend/contracts/TokenSwap.sol
+++ backend/contracts/TokenSwap.sol
@@ -5,28 +5,38 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@api3/contracts/v0.8/interfaces/IProxy.sol";
 
+/// @title Stable Token Exchange
+/// @dev Contract to exchange between ETH and a stable ERC20 token, using an external data feed for price.
 error StalePrice();
 error NoValue();
 
 contract TokenExchange is ERC20Burnable, Ownable {
+    
+    /// @notice Address of the external data feed proxy
     address public proxyAddress;
 
+    /// @dev Constructor initializes ERC20 token with name and symbol
     constructor() ERC20("Stable", "STA") {}
 
-    // Updating the proxy contract address is a security-critical
-    // action. In this example, only the owner is allowed to do so.
+    /// @notice Set the address of the data feed proxy contract
+    /// @dev Only callable by the contract owner
+    /// @param _proxyAddress The address of the data feed proxy
     function setProxyAddress(address _proxyAddress) public onlyOwner {
         proxyAddress = _proxyAddress;
     }
 
-    function readDataFeed() public view returns (uint256, uint256) {
+    /// @notice Reads the data from the proxy contract
+    /// @return price The price from the data feed
+    /// @return timestamp The timestamp of the last update from the data feed
+    function readDataFeed() public view returns (uint256 price, uint256 timestamp) {
         (int224 value, uint256 timestamp) = IProxy(proxyAddress).read();
         //convert price to UINT256
         uint256 price = uint224(value);
         return (price, timestamp);
     }
 
-    //Mint
+    /// @notice Deposit ETH and mint equivalent Stable tokens
+    /// @dev The amount of tokens minted is based on the current price feed
     function depositCollateral() external payable {
         if (msg.value == 0) revert NoValue();
         (uint256 price, uint256 time) = readDataFeed();
@@ -35,6 +45,9 @@ contract TokenExchange is ERC20Burnable, Ownable {
         _mint(msg.sender, amount);
     }
 
+    /// @notice Burn Stable tokens and reclaim equivalent ETH
+    /// @dev The amount of ETH sent is based on the current price feed
+    /// @param _amount The amount of Stable tokens to burn
     function reclaimEth(uint256 _amount) external {
         if (_amount == 0) revert NoValue();
         (uint256 price, uint256 time) = readDataFeed();
