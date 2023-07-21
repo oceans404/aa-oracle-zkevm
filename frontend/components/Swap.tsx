bb2ed3967f03b2f5450bbe6601ff2385e0d6884b
Steph <steph@oceans404.com>
2023-07-20 17:12:30 -0700

    style link

--- frontend/components/Swap.tsx
+++ frontend/components/Swap.tsx
@@ -7,27 +7,62 @@ import {
   CardFooter,
   Text,
   Heading,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
 } from "@chakra-ui/react";
 
-const Swap = (props: { priceFeed: number | null }) => {
+const Swap = (props: {
+  priceFeed: number | null;
+  maxEth: number | null;
+  maxSta: number | null;
+  tokenContract: string;
+}) => {
   return (
-    <Card my={4} p={4}>
+    <Card my={4} p={4} className={styles.swap}>
       <CardHeader>
-        <Heading>Swap Tokens</Heading>
-        <a
-          href="https://market.api3.org/dapis/polygon-zkevm-testnet/ETH-USD"
-          rel="noopener noreferrer"
-          target="_blank"
-          style={{ textDecoration: "underline" }}
-        >
-          ETH / USD price feed
-        </a>{" "}
-        - ${Number(props.priceFeed).toLocaleString("en")}
+        <Heading>STA stablecoin swap</Heading>
+        <Text>
+          API3{" "}
+          <a
+            href="https://market.api3.org/dapis/polygon-zkevm-testnet/ETH-USD"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            ETH / USD price feed
+          </a>{" "}
+          - ${Number(props.priceFeed).toLocaleString("en")}
+        </Text>
       </CardHeader>
       <CardBody>
-        <div></div>
-        <Text>swap swap swap</Text>
+        <Tabs isFitted variant="enclosed">
+          <TabList>
+            <Tab>ETH ➡️ STA</Tab>
+            <Tab>STA ➡️ ETH</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <p>ETH ➡️ STA</p>
+              <Text>ETH: {props.maxEth?.toString()}</Text>
+              <Text>STA: {props.maxSta?.toString()}</Text>
+            </TabPanel>
+            <TabPanel>
+              <p>STA ➡️ ETH</p>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
       </CardBody>
+      <CardFooter>
+        <a
+          href={`https://testnet-zkevm.polygonscan.com/address/${props.tokenContract}`}
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          👀 STA Contract
+        </a>
+      </CardFooter>
     </Card>
   );
 };
