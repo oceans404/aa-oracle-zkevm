8e62ed9ea7cb8a6b4231b266b29d8dadff3c448a
billyjitsu <82428978+billyjitsu@users.noreply.github.com>
2023-07-26 12:41:33 -0700

    Idea Push

--- frontend/components/BicononyAA.tsx
+++ frontend/components/BicononyAA.tsx
@@ -1,3 +1,28 @@
+import { writeContract, waitForTransaction } from "@wagmi/core";
+import styles from "../styles/Swap.module.css";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  Box,
+  Text,
+  Heading,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Input,
+  InputGroup,
+  InputLeftAddon,
+  InputRightAddon,
+  Spinner,
+} from "@chakra-ui/react";
+import { useForm, useWatch } from "react-hook-form";
+import { useState, useEffect } from "react";
+import { abi } from "../artifacts/contracts/TokenSwap.sol/TokenExchange.json";
+import { contractAddress, chainId } from "../artifacts/contractInfo";
+import { ethers } from "ethers";
 //New Imports
 import { IBundler, Bundler } from "@biconomy/bundler";
 import {
@@ -14,12 +39,32 @@ import {
   BiconomySmartAccountConfig,
   DEFAULT_ENTRYPOINT_ADDRESS,
 } from "@biconomy/account";
-import { ethers, Signer } from "ethers";
-import { useState, useEffect } from "react";
-import { abi } from "../artifacts/contracts/TokenSwap.sol/TokenExchange.json";
-import { contractAddress, chainId } from "../artifacts/contractInfo";
 //Ethers.js adapter
 import { useEthersProvider, useEthersSigner } from "./ethers";
+
+type AccountAbstractionProps = {
+  getUpdatedBalances: string | undefined,  
+  connectedAddress: string,
+  addressIsConnected: boolean,
+  priceFeed: number | undefined,  
+  maxEth: number | null,
+  maxSta: number | null,
+  tokenContract: string,    
+};
+
+type DataProps = {
+  ETH?: number;
+  STA?: number;
+};
+
+const calcEthToSta = (eth: number, price: number) => eth * price;
+
+const calcStaToEth = (sta: number, price: number) => {
+  if (sta) {
+    return sta / price;
+  }
+  return 0;
+};
 
 //Biconomy Setups
 const bundler: IBundler = new Bundler({
@@ -33,9 +78,68 @@ const paymaster: IPaymaster = new BiconomyPaymaster({
     "https://paymaster.biconomy.io/api/v1/1442/qwE3p3lk2.e29a50a3-2c5c-423e-99a4-958027e40a62", // you can get this value from biconomy dashboard.
 });
 
-const AccountAbstraction = () => {
+
+const AccountAbstraction: React.FC<AccountAbstractionProps> = ({
+  getUpdatedBalances,
+  connectedAddress,
+  addressIsConnected,
+  priceFeed,
+  maxEth,
+  maxSta,
+  tokenContract,
+}) => {
   const provider = useEthersProvider();
   const signer = useEthersSigner();
+
+  const [isLoading, setIsLoading] = useState(false);
+  const {
+    control,
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      ETH: Math.min(maxEth || 100, 1),
+      STA: 1,
+    },
+  });
+
+  const watchEth = useWatch({
+    control,
+    name: "ETH",
+  });
+
+  const watchSta = useWatch({
+    control,
+    name: "STA",
+  });
+
+  const setMaxEth = () => {
+    if (maxEth !== null) {
+    setValue("ETH", maxEth);
+    }
+  };
+
+  const setMaxSTA = () => {
+    if (maxSta !== null) {
+    setValue("STA", maxSta);
+    }
+  };
+
+  useEffect(() => {
+    setValue("ETH", Math.min(maxEth || 100, 1));
+  }, [maxEth]);
+
+  const onSwapEth = (data: DataProps) => {
+    // handleDepositEth(data.ETH);
+    sponsoredTransaction ();
+
+  };
+
+  const onSwapSta = (data: DataProps) => {
+   // handleWithdrawEth(data.STA);
+  };
 
   //In case there is no signer
   if (!signer) {
@@ -123,10 +227,162 @@ const AccountAbstraction = () => {
     }
   }
 
-  return(
-    <div>
-      <button onClick={sponsoredTransaction}>Sponsored Transaction</button>
-    </div>
+  return (
+    <Card my={4} p={4} className={styles.swap}>
+      <CardHeader>
+        <Heading>STA stablecoin swap</Heading>
+        <br></br>
+        {maxEth !== null && maxEth < 0.02 && (
+          <Text>
+            🚰 Get ETH from the{" "}
+            <a
+              href="https://faucet.polygon.technology/"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Polygon zkEVM Faucet
+            </a>
+          </Text>
+        )}
+        <Text>
+          API3{" "}
+          <a
+            href="https://market.api3.org/dapis/polygon-zkevm-testnet/ETH-USD"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            ETH / USD price feed
+          </a>{" "}
+          - ${Number(priceFeed).toLocaleString("en")}
+        </Text>
+        <Text>
+          <a
+            href={`https://testnet-zkevm.polygonscan.com/address/${tokenContract}`}
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            👀 STA Contract
+          </a>
+        </Text>
+      </CardHeader>
+      <CardBody>
+        <Tabs isFitted variant="enclosed">
+          <TabList>
+            <Tab>ETH ➡️ STA</Tab>
+            <Tab>STA ➡️ ETH</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <p>Swap ETH for STA</p>
+
+              <form onSubmit={handleSubmit(onSwapEth)}>
+                <InputGroup my={4}>
+                  <InputLeftAddon>ETH</InputLeftAddon>
+
+                  <Box>
+                    <Input
+                      type="decimal"
+                      placeholder="ETH"
+                      {...register("ETH", {})}
+                    />
+                    {watchSta && priceFeed !== undefined && (
+                      <Text px={"10px"}>
+                        ~
+                        {calcEthToSta(watchEth, priceFeed).toLocaleString("en")}{" "}
+                        STA
+                      </Text>
+                    )}
+                  </Box>
+                  <InputRightAddon
+                    onClick={setMaxEth}
+                    className="right-action"
+                    style={{
+                      backgroundColor: "white",
+                      color: "black",
+                      cursor: "pointer",
+                    }}
+                  >
+                    Max
+                  </InputRightAddon>
+                </InputGroup>
+
+                {isLoading ? (
+                  <Spinner />
+                ) : (
+                  <Input
+                    value={
+                      addressIsConnected ? "Swap" : "Connect wallet to swap"
+                    }
+                    disabled={!addressIsConnected}
+                    type="submit"
+                    style={{
+                      cursor: "pointer",
+                      backgroundColor: "rgb(14, 118, 253)",
+                      color: "white",
+                    }}
+                  />
+                )}
+              </form>
+            </TabPanel>
+            <TabPanel>
+              <p>Swap STA for ETH</p>
+              <form onSubmit={handleSubmit(onSwapSta)}>
+                <InputGroup my={4}>
+                  <InputLeftAddon>STA</InputLeftAddon>
+                  <Box>
+                    <Input
+                      type="decimal"
+                      placeholder="STA"
+                      {...register("STA", {})}
+                    />
+
+                    {watchSta && priceFeed !== undefined && (
+                      <Text px={"10px"}>
+                        ~
+                        {calcStaToEth(watchSta, priceFeed).toLocaleString("en")}{" "}
+                        ETH
+                      </Text>
+                    )}
+                  </Box>
+                  <InputRightAddon
+                    onClick={setMaxSTA}
+                    className="right-action"
+                    style={{
+                      backgroundColor: "white",
+                      color: "black",
+                      cursor: "pointer",
+                    }}
+                  >
+                    Max
+                  </InputRightAddon>
+                </InputGroup>
+
+                {isLoading ? (
+                  <Spinner />
+                ) : (
+                  <Input
+                    value={
+                      addressIsConnected ? "Swap" : "Connect wallet to swap"
+                    }
+                    disabled={!addressIsConnected}
+                    type="submit"
+                    style={{
+                      cursor: "pointer",
+                      backgroundColor: "rgb(14, 118, 253)",
+                      color: "white",
+                    }}
+                  />
+                )}
+              </form>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+
+        <br />
+        <Text>ETH balance: {maxEth?.toLocaleString("en")}</Text>
+        <Text>STA balance: {maxSta?.toLocaleString("en")}</Text>
+      </CardBody>
+    </Card>
   );
 };
 
